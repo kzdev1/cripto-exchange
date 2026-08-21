1a4c92f86c7d7c0b31b6ff58b592e8d5efa553ac
kzdev1 <kzdevcode1@gmail.com>
2026-08-21 19:25:55 -0300

    navbar feito

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,11 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">  
-    <p>Início</p>
-    <p>Sobre</p>
-    <p>Contato</p>
-    <p>Entrar</p>
+    <div className="flex flex-col items-center justify-center h-screen text-text-primary bg-panel">  
+      <h1 className="text-3xl font-bold">Acompanhe o mercado cripto em tempo real </h1>
+      <p className="text-lg">Preços, variações e comparações das principais criptomoedas, tudo em um só lugar.</p>
+      <input type="text" placeholder=" Buscar criptomoeda..." className=" bg-panel2 border border-border-custom placeholder:text-text-secondary w-90 rounded-lg" ></input>
     </div>
   );
 }
+
